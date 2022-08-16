3155d9fbbb461b811587c729c01e69a68ea3890f
unknown <Shafee Jan@shafee-pc.teo-intl.com>
2022-08-16 13:05:49 +0500

    add whole project

---
+++ phone/scripts/config-sample.js
@@ -0,0 +1,12 @@
+var user = {
+    //  User Name
+    "User" : "device.a23995751",
+    //  Password
+    "Pass" : "Arooj123",
+    //  Auth Realm
+    "Realm"   : "3kontaktpartnernr.dk",
+    // Display Name
+    "Display" : "Gob Bleuth",
+    // WebSocket URL
+    "WSServer"  : "wss://3frontoffice.nr.tre.se"
+};
